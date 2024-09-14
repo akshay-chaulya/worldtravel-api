87fb5be5160cf4c2dcf27dbaf1b8f0b35d5841c5
Akshay Chaulya <akshaychaulya09@gmail.com>
2024-09-14 16:14:26 +0530

    ""

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.get("/", (req, res) => {
 app.use("/api/v1", mainRouter);
 
 // Handle 404 errors (undefined routes)
-app.use((req, res, next) => {
-  res.status(404).json({ message: "Page not found" });
-});
+// app.use((req, res, next) => {
+//   res.status(404).json({ message: "Page not found" });
+// });
 
 // Universal error handler
 app.use((err, req, res, next) => {
