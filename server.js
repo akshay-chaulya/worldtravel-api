5f4d4e98b85fc73968b1c4d2104fd92fa221e112
Akshay Chaulya <akshaychaulya09@gmail.com>
2024-09-14 16:27:47 +0530

    I think error solved

--- server.js
+++ server.js
@@ -6,50 +6,26 @@ import { connectDB } from "./db/index.js";
 import mainRouter from "./routes/index.js";
 import { login } from "./controllers/auth.controller.js";
 
-// const corsOptions = {
-//   origin: ["https://worldtravel1.netlify.app", "http://localhost:5173"],
-//   methods: ["GET", "POST", "PUT", "DELETE"], // Add allowed methods
-//   credentials: true, // Allow credentials if needed
-// };
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      "https://worldtravel1.netlify.app",
-      "http://localhost:5173",
-    ];
-
-    // Allow requests with no 'origin' (e.g., mobile apps or curl requests)
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"], // Add any other headers you expect to send
+  origin: ["https://worldtravel1.netlify.app", "http://localhost:5173"],
 };
 
 const app = express();
 
 // CORS middleware
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Handle preflight requests if necessary
 // app.options("*", cors(corsOptions));
 
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.json({ message: "working" });
-});
+
 app.use("/api/v1", mainRouter);
-app.post("/api/v1/auth/login", login);
 
 // Handle 404 errors (undefined routes)
-// app.use((req, res, next) => {
-//   res.status(404).json({ message: "Page not found" });
-// });
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Page not found" });
+});
 
 // Universal error handler
 app.use((err, req, res, next) => {
