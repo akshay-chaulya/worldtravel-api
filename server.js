46bfed445fd663851e3bc3c29ef0b8e96f039fb0
akshay08test <akshaychaulya08@gmail.com>
2024-12-21 00:05:12 +0530

    .

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ import { login } from "./controllers/auth.controller.js";
 
 const allowlist = [
   "https://worldtravel-taupe.vercel.app",
-  "https://worldtravel-enooe7bx9-akshays-projects-a6b118a5.vercel.app/",
+  "https://worldtravel-o5mn3mzsa-akshays-projects-a6b118a5.vercel.app",
   "http://localhost:5173",
 ];
 const corsOptionsDelegate = function (req, callback) {
